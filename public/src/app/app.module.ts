3b60cfa3933af65f279577e7efe8af966607fbb5
eugene-benitez <eugenebenitez@protonmail.com>
2019-06-01 18:02:48 -0700

    Added Header Section and CSS animations Div.text-box.

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
+import { HomeComponent } from './common/components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from './common/services/http.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +14,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
